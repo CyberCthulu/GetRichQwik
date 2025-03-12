1ad443d839ed27600755f2075c166b82a2e8c267
vinal <vinchau01@gmail.com>
2025-03-11 21:29:54 -0700

    updated seeder undo

--- app/seeds/stocks.py
+++ app/seeds/stocks.py
@@ -1,6 +1,5 @@
-# app/seeds/seed_stocks.py
 from datetime import datetime
-from app.models import db, Stock
+from app.models import db, Stock, SCHEMA
 
 def seed_stocks():
     # List of curated stocks – add or remove entries as desired.
